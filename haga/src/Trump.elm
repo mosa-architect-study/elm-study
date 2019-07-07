067b8ec61ababdd49547b39c41e8fdd83ee715c5
hagaitski <i.haga629@gmail.com>
2019-07-07 18:02:54 +0900

    hagaがtrumpを作った

---
+++ haga/src/Trump.elm
@@ -0,0 +1,60 @@
+import Browser
+import Html exposing (Html, button, div, text,table,tr,td,tbody)
+import Html.Events exposing (onClick)
+import Random
+import Maybe
+import Random.List as RandomList
+import List.Extra as ListExtra
+
+main =
+  Browser.element
+    { init = init
+    , update = update
+    , subscriptions = subscriptions
+    , view = view
+    }
+
+type alias CardValue = {mark:Mark,number:Int}
+type Mark = H | S | C | D
+type Card = Joker | NomalCard CardValue
+type alias Model = (List Card,List Card)
+type Msg = Draw | ShuffleResult (List Card)
+
+trump : List Card
+trump = ListExtra.lift2 CardValue [H,S,C,D] (List.range 1 13) |> List.map NomalCard |> List.append [Joker,Joker]
+
+init : () -> (Model, Cmd Msg)
+init _ = (([],[]), Random.generate ShuffleResult (RandomList.shuffle trump))
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg (deck,discard) = 
+    case msg of
+        ShuffleResult suffuled -> ((suffuled,discard),Cmd.none)
+        Draw -> ((deck,discard) |> move |> Maybe.withDefault (deck,discard),Cmd.none)
+
+move : (List a,List a) -> Maybe (List a,List a)
+move (from,to) = Maybe.map2 (\head -> \tail -> (tail,head :: to)) (List.head from) (List.tail from)
+
+view : Model -> Html Msg
+view (deck,discard) =
+  div [] [
+      button [onClick Draw] [text "ドロー"],
+      div [] (List.map cardView discard)
+  ]
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
+
+cardView : Card -> Html Msg
+cardView card = 
+    case card of 
+        Joker -> div [] [text "Joker"]
+        NomalCard {mark,number} -> div [] [text <| ((++) <| markToText <| mark) <| (++) "の" <| String.fromInt <| number]
+ 
+markToText : Mark -> String
+markToText mark = 
+    case mark of 
+        D -> "ダイヤ"
+        C -> "クローバー"
+        S -> "スペード"
+        H -> "ハート"
