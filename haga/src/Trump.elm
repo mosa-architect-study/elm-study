34013031a18e1cc338cb8fa28359c960827dd291
hagaitski <i.haga629@gmail.com>
2019-07-07 18:16:00 +0900

    コメント書いた

--- haga/src/Trump.elm
+++ haga/src/Trump.elm
@@ -14,36 +14,39 @@ main =
     , view = view
     }
 
+-- モデル
 type alias CardValue = {mark:Mark,number:Int}
 type Mark = H | S | C | D
 type Card = Joker | NomalCard CardValue
 type alias Model = (List Card,List Card)
 type Msg = Draw | ShuffleResult (List Card)
 
+-- カードセットを初期化
 trump : List Card
 trump = ListExtra.lift2 CardValue [H,S,C,D] (List.range 1 13) |> List.map NomalCard |> List.append [Joker,Joker]
 
+-- init
 init : () -> (Model, Cmd Msg)
 init _ = (([],[]), Random.generate ShuffleResult (RandomList.shuffle trump))
 
+-- update
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg (deck,discard) = 
     case msg of
         ShuffleResult suffuled -> ((suffuled,discard),Cmd.none)
         Draw -> ((deck,discard) |> move |> Maybe.withDefault (deck,discard),Cmd.none)
 
+-- fromのリストから1枚をtoのリストに移す。Maybeなのはtoのリストが空の時
 move : (List a,List a) -> Maybe (List a,List a)
 move (from,to) = Maybe.map2 (\head -> \tail -> (tail,head :: to)) (List.head from) (List.tail from)
 
+-- view
 view : Model -> Html Msg
 view (deck,discard) =
   div [] [
       button [onClick Draw] [text "ドロー"],
       div [] (List.map cardView discard)
   ]
-
-subscriptions : Model -> Sub Msg
-subscriptions _ = Sub.none
 
 cardView : Card -> Html Msg
 cardView card = 
@@ -58,3 +61,6 @@ markToText mark =
         C -> "クローバー"
         S -> "スペード"
         H -> "ハート"
+
+subscriptions : Model -> Sub Msg
+subscriptions _ = Sub.none
